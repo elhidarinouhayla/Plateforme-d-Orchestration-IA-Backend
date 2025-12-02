f63c85dce9467ad19515e5bf8f37e9d29af48e74
elhidarinouhayla <elhidarinouhayla@gmail.com>
2025-12-02 09:02:53 +0100

    creation du BaseModel et shemas

---
+++ app/model.py
@@ -0,0 +1,19 @@
+from database import Base
+from pydantic import BaseModel
+from sqlalchemy import String, Integer, Column
+
+
+class User():
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, nullable=False )
+    password = Column(String, nullable=False)
+
+
+class UserCreate(BaseModel):
+    id : int
+    username : str
+    password : str
+
+class UserResponse(UserCreate):
+    id : int
