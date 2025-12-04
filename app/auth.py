6f3c688c828c83b9cdc05a340a7e5a1097cacc85
elhidarinouhayla <elhidarinouhayla@gmail.com>
2025-12-04 20:45:20 +0100

    creation du token

---
+++ app/auth.py
@@ -0,0 +1,16 @@
+from jose import jwt
+from fastapi import HTTPException, Header
+from config import SECRET_KEY, ALGORITHM
+
+
+def create_token(username:str):
+    paylod = {
+        "sub": username
+        }
+    token = jwt.encode(paylod,SECRET_KEY,algorithm=ALGORITHM)
+    return token
+
+
+
+
+    
