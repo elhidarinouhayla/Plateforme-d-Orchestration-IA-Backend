c18052feb6515a286b5e21d9f2b7ee5f5065d6c4
elhidarinouhayla <elhidarinouhayla@gmail.com>
2025-12-07 17:44:32 +0100

    correction de erreurs

--- app/services/classification.py
+++ app/services/classification.py
@@ -15,12 +15,11 @@ def classifier(input_text, categories):
     def query(payload):
         response = requests.post(API_URL, headers=headers, json=payload)
         return response.json()
-
     output = query({
         "inputs": input_text,
         "parameters": {"candidate_labels": categories},
     })
 
-    return output
+    return output[0]
 
 # print(classifier("L'équipe a dominé le match grâce à une possession de balle remarquable et un pressing constant.", ["sport","sante","nouriture"]))
