14acae15dcc7ce00f5e25846de6c48f6ddf25e0e
elhidarinouhayla <elhidarinouhayla@gmail.com>
2025-12-07 17:52:11 +0100

    lier la sortie de hagging face avec gemini_service

--- app/services/connection.py
+++ app/services/connection.py
@@ -1,28 +1,57 @@
 from classification import classifier
 from gemini_service import analyze_with_gemini
 import json
+from pydantic import BaseModel
+
+
+class AnalyzeSchema(BaseModel):
+    resume : str
+    ton : str
+    categorie : str
+    score : float
+
 
 def classifier_and_analyze(text,categorie):
     HF_output = classifier(text, categorie)
 
-    HF_json = json.dumps(HF_output)
-
+   
+    categorie = HF_output["label"]
+    score = HF_output["score"]
+   
+  
     prompt = f"""
 text_original = "{text}"
 
-Analyse le JSON suivant produit par Haggimg Face:
-
-{HF_json}
+Catégorie détectée par Hugging Face : "{categorie}"
+Score : {score}
 
 Donne moi :
 - un resume tres court
 - un ton general : positif , negatif ou neutre
+- retourne exactement le JSON suivant :
+{{
+    "resume": "...",
+    "resume": "...",
+    "ton": "...",
+    "categorie": "{categorie}",
+    "score": {score}
+    
+    }}
 """
     
-    return analyze_with_gemini(prompt)
+    gemini_result = analyze_with_gemini(prompt)
+
+    output = AnalyzeSchema(
+        resume=gemini_result.resume,
+        ton=gemini_result.ton,
+        categorie=categorie,
+        score=score
+    )
+
+    return output
 
 text = "L'équipe a dominé le match grâce à une possession de balle remarquable et un pressing constant."
-categorie = ["sante", "nouriture", "sport", "beaute", "fashion"]
+categorie = ["sante", "nouriture", "sport", "beaute", "fashion", "technologie"]
 
 result = classifier_and_analyze(text,categorie)
 
